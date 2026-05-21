9b57517b0835dec2d5ad52a04562789ed421f939
Sanmaya Mohanty <sanmayamohanty@gmail.com>
2026-05-21 21:29:44 +0530

    Configure main.jsx entry point with AppProvider and HashRouter

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import { AppProvider } from './context/AppContext';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </AppProvider>
+  </React.StrictMode>,
+);
